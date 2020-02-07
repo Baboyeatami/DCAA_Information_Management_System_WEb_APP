b72d485371253b513e87266990d414befa287892
Jamie <Jamie@DESKTOP-RCLMTNF>
2020-02-07 13:44:36 +0800

    inititial Build

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>DCAA Billing and Management System </title>
+    </head>
+    <body>
+        <div>
+            <h1>  Student Registration </h1>
+
+            <form>
+                First Name: <input type="text" name="Firstname">
+                Middle Name: <input type="text" name="Middlename">
+                Last Name: <input type="text" name="Lastname">
+                Name Ex: <input type="text" name="Name_Ex">
+                <br>
+                <br>
+                Birthday: <input type="date" name="bday">
+                Gender:
+                <select>
+                    <option name="Gender"
+                <option value="Male"> Male </option>
+                <option value="Female"> Female </option>
+
+            </select>
+
+            Enrollment Status:
+            <select>
+                <option name="Enrollment Status"
+            <option value="New_Enroll"> New  </option>
+            <option value="Returnee"> Returnee </option>
+            <option value="Transferee"> Transferee </option>
+
+        </select>
+        <br>
+        Birth Place: <input type="text" name=""  size="70">
+
+    </form>
+
+</div>
+
+
+<?php
+?>
+</body>
+</html>
